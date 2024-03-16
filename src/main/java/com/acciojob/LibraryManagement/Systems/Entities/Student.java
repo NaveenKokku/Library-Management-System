fb44b6e751d59edaebf9a29201404095f1c31506
NaveenKokku <kokkunaveen98@gmail.com>
2024-03-16 11:25:03 +0530

    first LMS commit

---
+++ src/main/java/com/acciojob/LibraryManagement/Systems/Entities/Student.java
@@ -0,0 +1,27 @@
+package com.acciojob.LibraryManagement.Systems.Entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Getter //lombok annotations for simplyfing code it will create all the getters, setters, cons
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "student info")
+
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int rollNo;
+    private String name;
+    private String branch;
+    private Double cgpa;
+    private String emailId;
+
+}
---
+++ src/test/java/com/acciojob/LibraryManagement/Systems/LibraryManagementSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acciojob.LibraryManagement.Systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
